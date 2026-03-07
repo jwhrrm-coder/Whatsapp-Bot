0743dd5536728480faa70dbcd70e89e4643d42de
Hariswar8018 <hariswarsamasi@gmail.com>
2026-03-07 13:51:10 +0530

    fix

--- app/firebase_client.py
+++ app/firebase_client.py
@@ -1,12 +1,14 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
 import os
 import json
+import firebase_admin
+from firebase_admin import credentials, firestore
 
-# load service account from env variable
 cred_dict = json.loads(os.getenv("FIREBASE_SERVICE_ACCOUNT"))
 
+cred_dict["private_key"] = cred_dict["private_key"].replace("\\n", "\n")
+
 cred = credentials.Certificate(cred_dict)
+
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
